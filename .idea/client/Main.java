60293fddb69dc21ffbd435386efafd4c4e8563ab
K1mYoungMan <puduing@naver.com>
2023-12-14 15:21:37 +0900

    (C3) 서버와 클라이언트 공용으로 쓸 디렉토리 구조 생성

---
+++ .idea/client/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "클라이언트용 프로그램");
+    }
+}
---
+++ .idea/server/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "서버용 프로그램");
+    }
+}
